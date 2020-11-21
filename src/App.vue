332ed13bea66d7328f4caca070f1b2292b9aba1a
cchura94 <cchura.cpc@gmail.com>
2020-11-20 22:12:46 -0400

    app actividads

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Inicio</router-link> |
-    <router-link to="/about">Acerca de Nosotros</router-link> |
-    <router-link to="/actividad">Actividad</router-link> |
-    <router-link to="/contacto">Contactenos</router-link> |
-    <router-link to="/blog">Blog</router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <a class="navbar-brand" href="#">EMPRESA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">INICIO <span class="sr-only">(current)</span></router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">ACERCA DE NOSOTROS</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/actividad">ACTIVIDADES</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/contacto">CONTACTO</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/blog">BLOG</router-link>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
   </div>
-  <router-view/>
+</nav>
+
+  <div class="container">
+    <router-view/>
+  </div>
+  
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,6 +1,69 @@
-<template>
-  
+<template>  
   <h1>{{ titulo }}</h1>
+
+  <div class="row">
+    <div class="col-md-4">
+      <h1>Nueva Actividad</h1>
+      <form >
+        <label for="">Ingrese Titulo</label>
+        <input type="text" class="form-control" v-model="act.titulo">
+
+        <label for="">Ingrese Detalle</label>
+        <textarea id="" class="form-control" v-model="act.detalle"></textarea>
+
+        <button type="button" class="btn btn-success float-right" @click="guardarAct()">Guardar Actividad</button>
+      </form>
+      {{ act }}
+    </div>
+    <div class="col-md-8">
+      <h1>Lista de Actividades</h1>
+
+      <table class="table table-striped table-hover">
+        <tr>
+          <td>ID</td>
+          <td>NOMBRE</td>
+          <td>DETALLE</td>
+          <td>ACCIONES</td>
+        </tr>
+
+        <tr v-for="(actividad, index) in actividades" :key="index">
+          <td>{{ index }}</td>
+          <td>{{ actividad.titulo }}</td>
+          <td>{{ actividad.detalle }}</td>
+          <td>
+            <button class="btn btn-outline-primary btn-xs" @click="enproceso(actividad)">EMPEZAR</button>
+            <button  class="btn btn-danger btn-xs" @click="eliminarAct(index)">x</button>
+
+          </td>
+        </tr>
+      </table>
+
+    </div>
+
+    
+  </div>
+
+<div class="row">
+      <div class="col-md-4 bg-warning text-light">
+        <h3>En PROCESO</h3>
+        
+        <div class="card" v-for="(a, index) in actividades" :key="index">
+          <div class="titulo" v-if="a.estado === 1">
+              <h1>{{ a.titulo}}</h1>
+          </div>          
+        </div>
+      </div>
+      <div class="col-md-4 bg-success  text-light">
+        <h3>FINALIZADO</h3>
+      </div>
+      <div class="col-md-4 bg-danger text-light">
+        <h3>CANCELADO</h3>
+      </div>
+
+    </div>
+
+
+
   <h4>Cantidad de actividades: {{ numero }}</h4>
   <input type="text" v-model="numero">
   <button @click="aumentar">+</button>
@@ -21,7 +84,10 @@ export default {
     return {
       titulo: "ACTIVIDADES",
       numero: 14,
-      nombres: ["Juan", "Cristian", "Ana", "Simon"]
+      nombres: ["Juan", "Cristian", "Ana", "Simon"],
+
+      actividades: [],
+      act: {titulo:"", detalle:"", estado: 0}
     }
   },
   methods:{
@@ -30,6 +96,26 @@ export default {
     },
     reducir(){
       this.numero--;
+    },
+    guardarAct(){
+      var aux = this.act
+      this.reset();
+      this.actividades.push(aux)
+    },
+    reset(){
+      this.act = {titulo:"", detalle:""}
+    },
+    eliminarAct(posicion){
+      this.actividades.splice(posicion, 1);
+    },
+    enproceso(actividad){
+      actividad.estado = 1;
+    },
+    finalizado(actividad){
+      actividad.estado = 2;
+    },
+    cancelado(actividad){
+      actividad.estado = 3;
     }
   }
 }
